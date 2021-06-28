59afbb7f365bc0fd6214aca53566f38a11f0911d
Azeera4 <86347709+Azeera4@users.noreply.github.com>
2021-06-28 11:45:41 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return(
+    <Footer>
+      <p>opyright by ShapeAI @ {new Date().getFullYear}</p>
+    </Footer>
+  );
+}
+export default Footer ;
